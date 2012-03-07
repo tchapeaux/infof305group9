a36df28397cd991205d0dbd1ad46605730d0e718
BMayeurMCM@gmail.com <BMayeurMCM@gmail.com@b39f4227-530b-c70e-2ddf-1518cdc1cbcb>
2012-03-07 16:54:19 +0000

    reste human generator
    configuration des simu et choix des batch ok

--- GeneratorPanel.java
+++ GeneratorPanel.java
@@ -1,9 +1,10 @@
 
-import java.awt.Color;
+import java.awt.Checkbox;
 import java.awt.Graphics;
-import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Stack;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -12,38 +13,109 @@ public class GeneratorPanel extends JPanel implements ActionListener {
 
     protected JButton randomGen = new JButton("Generate Random batch");
     protected JButton fileGen = new JButton("Generate batch from file");
+    protected JButton confirm = new JButton("Confirm");
     protected Panel father;
+    protected Checkbox SmallestPath = new Checkbox("Smallest Path Scheduler", null, true);
+    protected Checkbox SingleFreq = new Checkbox("Single Frequency Scheduler", null, true);
+    protected Checkbox DumbSched = new Checkbox("Dumb Scheduler", null, true);
+    protected Checkbox HumanSched = new Checkbox("Human Scheduler", null, false);
 
     GeneratorPanel(Panel father)
     {
-        this.setBackground(Color.white);
         this.father=father;
-        this.setLayout(new GridLayout(10,1));
+        this.setLayout(null);
+        Insets insets = this.getInsets();
 
         randomGen.setActionCommand("random");
-        randomGen.setVisible(true);
         randomGen.addActionListener(this);
         this.add(randomGen);
 
+        randomGen.setBounds(1200/2 -325 + insets.left, 10 + insets.top,
+             300, 25);
+        
+        
         fileGen.setActionCommand("fromFile");
-        fileGen.setVisible(true);
         fileGen.addActionListener(this);
         this.add(fileGen);
+        
+        fileGen.setBounds(1200/2 + 25 + insets.left, 10 + insets.top,
+             300, 25);
+        
+        confirm.setActionCommand("confirm");
+        confirm.addActionListener(this);
+        this.add(confirm);
+        
+        confirm.setBounds(1200 - 350 + insets.left, 800 - 75 + insets.top,
+             300, 25);
+        
+        SmallestPath.setBounds(250 + insets.left, 120 + insets.top,
+             300, 25);
+        SingleFreq.setBounds(250 + insets.left, 150 + insets.top,
+             300, 25);
+        HumanSched.setBounds(250 + insets.left, 180 + insets.top,
+             300, 25);
+        DumbSched.setBounds(550 + insets.left, 120 + insets.top,
+             300, 25);
+        this.add(SmallestPath);
+        this.add(SingleFreq);
+        this.add(HumanSched);
+        this.add(DumbSched);
+        
+        randomGen.setVisible(true);
+        fileGen.setVisible(true);
+        SmallestPath.setVisible(true);
+        confirm.setVisible(true);
+        
+        this.repaint();
     }
 
     @Override
 	public void paintComponent(Graphics g){
-        g.drawString("Generation des taches à ordonnancer", this.getWidth()/2-25, 10);
+            g.drawString("Choose your parameters :", this.getWidth()/2-80, 50);
+            g.drawString("Choose "+Main.NUMBER_OF_SIMS+" algorithms :", 200, 100);
+            g.drawString("If you choose Human Scheduler, ordonize the tasks :", 200, 250);
+            
+            drawTasks(father.getTasks(),g);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if ("random".equals(e.getActionCommand()))
-            father.switchView();
-        if ("fromFile".equals(e.getActionCommand()))
+        {
+            father.generateRandomBatch();
+            randomGen.setEnabled(false);
+            fileGen.setEnabled(true);
+            this.repaint();
+        }
+        else if ("fromFile".equals(e.getActionCommand()))
 	{
 	    father.generateBatchFromFile();
-	    father.switchView();
+            fileGen.setEnabled(false);
+            randomGen.setEnabled(true);
+            this.repaint();
 	}
+        else if ("confirm".equals(e.getActionCommand()))
+        {
+            Stack pushMe = new Stack();
+            if (SmallestPath.getState()) pushMe.push(new SmallestPathScheduler());
+            if (SingleFreq.getState()) pushMe.push(new SingleFrequencyScheduler());
+            if (DumbSched.getState()) pushMe.push(new DumbScheduler());
+            if (HumanSched.getState()) new UnsupportedOperationException("Not yet implemented");//pushMe.push(new HumanScheduler());
+            
+            if (pushMe.size() == Main.NUMBER_OF_SIMS)
+            {
+                Simulation[] simulations = new Simulation[Main.NUMBER_OF_SIMS];
+
+                simulations[2] = new Simulation((Scheduler)pushMe.pop(), father.getTasks());
+                simulations[1] = new Simulation((Scheduler)pushMe.pop(), father.getTasks());
+                simulations[0] = new Simulation((Scheduler)pushMe.pop(), father.getTasks());
+                father.setSimulations(simulations);
+                father.switchView();
+            }
+        }
+    }
+
+    private void drawTasks(Task[] tasks, Graphics g) {
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 }
